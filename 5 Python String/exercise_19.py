e78cb7d05a7ddf3e08d115a2f01df474f2411199
buildscript-dev <buildscript.dev@gmail.com>
2026-04-14 14:12:43 +0530

    Merge remote-tracking branch 'origin/main'

--- 5 Python String/exercise_19.py
+++ 5 Python String/exercise_19.py
@@ -1,1 +1,12 @@
-# Exercise 19 - 
+# Exercise 19. Create a mixed string using alternating characters
+
+s1 = "Abc"
+s2 = "Xyz"
+s3 = ""
+
+s2 = s2[::-1]
+
+for i in range(len(s1)):
+    s3 += s1[i] + s2[i]
+
+print(s3)
