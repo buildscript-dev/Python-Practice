23c61ffab70a03bbd6b1c1b5edd369216e532ae9
buildscript-dev <buildscript.dev@gmail.com>
2026-04-14 13:28:59 +0530

    Merge remote-tracking branch 'origin/main'

--- 5 Python String/exercise_16.py
+++ 5 Python String/exercise_16.py
@@ -1,1 +1,27 @@
-# Exercise 16 - 
+# Exercise 16 -
+
+filename = input("Enter the filename: ")
+
+# Extract extension
+file_extension = "." + filename.split(".")[-1]
+
+file_types = [
+    ".py", ".pyw", ".pyc", ".pyo",
+    ".env", ".ini", ".cfg", ".toml",
+    ".yaml", ".yml", ".json", ".xml",
+    ".html", ".css", ".js", ".jsx",
+    ".ts", ".tsx",
+    ".csv", ".xlsx", ".txt",
+    ".db", ".sqlite3", ".parquet",
+    ".ipynb", ".pkl", ".joblib", ".h5",
+    ".png", ".jpg", ".jpeg", ".gif",
+    ".svg", ".mp4", ".avi", ".mp3", ".wav",
+    ".md", ".rst",
+    ".sh", ".bat", ".log", ".tmp", ".bak"
+]
+
+# Check if extension is supported
+if file_extension in file_types:
+    print("You can access the file now....")
+else:
+    print("File Type Not Supported...")
