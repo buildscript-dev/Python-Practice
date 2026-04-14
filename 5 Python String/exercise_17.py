f572b5d2950805d30bf015cdd739f36ebb05215d
buildscript-dev <buildscript.dev@gmail.com>
2026-04-14 13:56:29 +0530

    Merge remote-tracking branch 'origin/main'

--- 5 Python String/exercise_17.py
+++ 5 Python String/exercise_17.py
@@ -1,2 +1,11 @@
 # Exercise 17. Lowercase First
 
+user_input =input("Enter any String: ")
+lower_string=""
+upper_string=""
+for i in user_input:
+    if i.islower():
+        lower_string += i
+    elif i.isupper():
+        upper_string += i
+print(lower_string + upper_string)
--- 5 Python String/exercise_18.py
+++ 5 Python String/exercise_18.py
@@ -1,1 +1,22 @@
-# Exercise 18 - 
+# Exercise 18. Count all letters, digits, and special symbols from a given string
+
+
+strings = "P@#yn26at^&i5ve"
+
+letter = 0
+digit = 0
+symbol = 0
+
+for i in strings:
+    if i.isalpha():
+        letter += 1
+    elif i.isdigit():
+        digit += 1
+    elif i == " " or i == "":
+        continue
+    else:
+        symbol += 1
+
+print("Total counts of chars, digits, and symbols: Chars = {} Digits = {} Symbol = {}".format(letter, digit, symbol))
+
+
