35761b53e07a505a7617938fb286ec61195500b3
buildscript-dev <buildscript.dev@gmail.com>
2026-04-15 20:21:27 +0530

    Merge remote-tracking branch 'origin/main'

--- 7 Python Function/exercise_7.py
+++ 7 Python Function/exercise_7.py
@@ -1,1 +1,15 @@
-# Exercise 7 - 
+# Exercise 7. Assign a Different Name to Function and Call It
+
+def display_student(name, rollNum):
+    rollNum = str(rollNum)
+
+    if len(rollNum) == 1:
+        rollNum = "0" + rollNum
+    print("Hey, {} your roll no. is {}".format(name, rollNum))
+
+
+def show_student(name, rollNum):
+    display_student(name, rollNum)
+
+
+show_student("Build", 77)
--- 7 Python Function/exercise_8.py
+++ 7 Python Function/exercise_8.py
@@ -1,1 +1,11 @@
-# Exercise 8 - 
+# Exercise 8. Generate a List of Even Numbers (Range Function)
+
+def even_num(num):
+    lst = []
+    for i in range(1, num+1):
+        if i % 2 == 0:
+            lst.append(i)
+    print(lst)
+result = even_num(20)
+
+# print(result)
